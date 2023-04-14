f385bf05304de1853eef11b1169e0169634aeeb1
Whites4I <andriypetrina2000@gmail.com>
2023-04-14 21:22:48 +0300

    2 task ready

--- Task_6/main.js
+++ Task_6/main.js
@@ -9,7 +9,6 @@
 
 class CssClass {
 
-
     //назва css класса; масив стилів;
     constructor(nameCss) {
         this.nameCss = nameCss;
@@ -34,17 +33,60 @@ class CssClass {
             css += element + ': ' + this.styles[element] + '; ';
         });
 
-        css += `}`
-
-        return css
+        css += `}`;
+        return css;
     }
 }
 
-const Button = new CssClass('button');
+const Overlay = new CssClass('form');
 
-Button.setStyles('background-color', 'green')
-Button.setStyles('margin', 0)
-console.log(Button)
-// Body.deleteStyles = 'background-color';
-console.log(Button.styles)
-console.log(Button.getCSS)
+Overlay.setStyles('background-color', 'green');
+Overlay.setStyles('margin', 0);
+console.log(Overlay);
+// Overlay.deleteStyles = 'background-color';
+console.log(Overlay.getCSS);
+
+/*
+2. Реалізувати клас Button, який містить ширину, висоту, текст кнопки і метод showBtn(), 
+який виводить кнопку на екран за допомогою тега button і функції document.write().
+Реалізувати клас BootstrapButton, успадкувавши його від класу Button. 
+Додати поле color і перевизначити метод showBtn() так, щоб кнопка виводилася зі стилями та вказаним кольором.
+*/
+
+class Button extends CssClass {
+    constructor(width, height, nameCss, nameButton) {
+        super(nameCss);
+        this.nameButton = nameButton;
+        this.setStyles('width', `${width}px`);
+        this.setStyles('height', `${height}px`);
+    }
+
+    get getCSS() {
+        let css = ``;
+
+        Object.keys(this.styles).forEach(element => {
+            css += element + ': ' + this.styles[element] + '; ';
+        });
+
+        return css;
+    }
+
+    showBtn() {
+        const getButtonId = document.getElementById('button');
+        getButtonId.innerHTML = `<button style="${this.getCSS}">${this.nameButton}</button>`;
+        // document.write(`<button style="${this.getCSS}">${this.nameButton}</button>`);
+    }
+}
+
+const buttonWrite = new Button(100, 50, 'button', 'ClickOnMe');
+buttonWrite.showBtn();
+
+class BootstrapButton extends Button {
+    constructor(width, height, nameCss, nameButton, color) {
+        super(width, height, nameCss, nameButton)
+        this.setStyles('color', `${color}`)
+    }
+}
+
+const blueButton = new BootstrapButton(100, 50, 'blueButton', 'I am a blue button', 'blue');
+blueButton.showBtn();
