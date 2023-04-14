fbdf3d9f273f34b799e35808cde56b549203ca2d
Whites4I <andriypetrina2000@gmail.com>
2023-04-14 19:12:27 +0300

    First task ready

--- Task_6/main.js
+++ Task_6/main.js
@@ -8,10 +8,43 @@
 */
 
 class CssClass {
-    constructor(name, style) {
-        this.name = name;
-        this.style = style;
+
+
+    //назва css класса; масив стилів;
+    constructor(nameCss) {
+        this.nameCss = nameCss;
+        this.styles = [];
     }
 
+    //метод для встановлення стилю;
+    setStyles(property, values) {
+        this.styles[property] = values;
+    }
 
+    //метод для видалення стилю;
+    set deleteStyles(property) {
+        delete this.styles[property];
+    }
+
+    //метод getCSS(), який повертає CSS код у вигляді рядка.
+    get getCSS() {
+        let css = `.${this.nameCss} {`
+
+        Object.keys(this.styles).forEach(element => {
+            css += element + ': ' + this.styles[element] + '; ';
+        });
+
+        css += `}`
+
+        return css
+    }
 }
+
+const Button = new CssClass('button');
+
+Button.setStyles('background-color', 'green')
+Button.setStyles('margin', 0)
+console.log(Button)
+// Body.deleteStyles = 'background-color';
+console.log(Button.styles)
+console.log(Button.getCSS)
