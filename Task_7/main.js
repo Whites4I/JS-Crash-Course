535e1f02eeb837889f70dc890b008b8d354ebfc7
Whites4I <andriypetrina2000@gmail.com>
2023-04-18 15:52:34 +0300

    Ready second task

--- Task_7/main.js
+++ Task_7/main.js
@@ -21,3 +21,24 @@ setInterval(
         }
         time.clockInner();
     }, 1000);
+
+/*
+2. Розробити сторінку, яка «роздаватиме» ігральні карти двом гравцям, кожному по 6 штук. 
+Тип карти обирається випадковим чином. Відобразити на сторінці карти кожного гравця. Дизайн на ваш розсуд…)
+*/
+
+function playCards(player, numberCards) {
+    const cards = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'T'];
+    const cardId = document.getElementById(player);
+    for (let i = 1; i <= numberCards; i++) {
+        const card = document.createElement('div');
+        const cardSuit = cards[(Math.floor(Math.random() * cards.length))];
+        card.className = 'card';
+        card.innerHTML = `<h3>${cardSuit}</h3><h4>${cardSuit}</h4>`;
+        cardId.appendChild(card);
+    }
+    console.log(cardId);
+}
+
+playCards('player1', 6);
+playCards('player2', 6);
